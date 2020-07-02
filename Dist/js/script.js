b2fb981900f39b450b9ef5dc3a42bb927e3b8372
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-02 19:03:19 +0200

    Full responsivness

--- Dist/js/script.js
+++ Dist/js/script.js
@@ -3,14 +3,23 @@ const mainNav = document.getElementById('main-nav');
 
 const init = () => {
     const swup = new Swup();
+
 };
+
+init();
+
+// Adjust navigation class depend on window width
+window.addEventListener('resize', () => {
+    if (innerWidth > 1025) {
+        mainNav.classList.remove('show');
+        menuBtn.classList.remove('close');
+    };
+})
 
 // Show/hide navigation menu
 menuBtn.addEventListener('click', () => {
     mainNav.classList.toggle('show');
     menuBtn.classList.toggle('close');
 });
-
-init();
 
 document.addEventListener('swup:contentReplaced', init);
