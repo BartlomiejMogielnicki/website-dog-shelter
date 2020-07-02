9960615b7b3473e8647171bf4de0947db9c683e8
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-02 15:06:08 +0200

    Cut down images size. Add responsivness for small devices. Add hamburger menu button and hide/show menu functionality.

--- Dist/js/script.js
+++ Dist/js/script.js
@@ -1,1 +1,16 @@
-const swup = new Swup();
+const menuBtn = document.getElementById('menu-btn');
+const mainNav = document.getElementById('main-nav');
+
+const init = () => {
+    const swup = new Swup();
+};
+
+// Show/hide navigation menu
+menuBtn.addEventListener('click', () => {
+    mainNav.classList.toggle('show');
+    menuBtn.classList.toggle('close');
+});
+
+init();
+
+document.addEventListener('swup:contentReplaced', init);
