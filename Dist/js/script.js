58d81db9fa2dba8069ffd9e56cddd27453e83006
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-03 14:44:33 +0200

    Main navigation fix to screen on small devices

--- Dist/js/script.js
+++ Dist/js/script.js
@@ -1,9 +1,9 @@
 const menuBtn = document.getElementById('menu-btn');
 const mainNav = document.getElementById('main-nav');
+const homeBtn = document.getElementById('home-btn');
 
 const init = () => {
     const swup = new Swup();
-
 };
 
 init();
@@ -21,5 +21,11 @@ menuBtn.addEventListener('click', () => {
     mainNav.classList.toggle('show');
     menuBtn.classList.toggle('close');
 });
+
+// Hide main navigation if swap to home page
+homeBtn.addEventListener('click', () => {
+    mainNav.classList.remove('show');
+    menuBtn.classList.remove('close');
+})
 
 document.addEventListener('swup:contentReplaced', init);
